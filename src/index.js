9fcbc39ce15a08e7c0af0e1d764d1f9be0a18f37
Rkay Ji <mrrkayj86@gmail.com>
2025-10-01 21:53:33 +0530

    index updated with origin

--- src/index.js
+++ src/index.js
@@ -7,14 +7,28 @@ import rateLimit from 'express-rate-limit';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const allowedORIGIN = process.env.FRONTEND_URL || 'http://localhost:3000';
+const allowedORIGINS = [
+    process.env.FRONTEND_URL || 'http://localhost:3000',
+    'http://localhost:5173', // Add more origins as needed,
+    'https://portfolio-alpha-indol-61.vercel.app',
+    'https://portfolio-git-development-rkayjis-projects.vercel.app',
+    'https://portfolio-d7mpujgfx-rkayjis-projects.vercel.app'
+];
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
     max: 100
 });
 
 app.use(cors({
-    origin: allowedORIGIN
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps, curl, etc.)
+        if (!origin) return callback(null, true);
+        if (allowedORIGINS.includes(origin)) {
+            return callback(null, true);
+        } else {
+            return callback(new Error('Not allowed by CORS'));
+        }
+    }
 }));
 app.use(express.json());
 app.use(helmet());
